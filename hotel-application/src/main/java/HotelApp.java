a51893b611e4a5a1405bbe8cd7feaab38163e225
Ayanna Williams <ayanna.williams20@att.net>
2024-10-23 11:29:54 -0400

    initial hotel application commit

---
+++ hotel-application/src/main/java/HotelApp.java
@@ -0,0 +1,8 @@
+public class HotelApp {
+    public static void main(String [] args){
+        boolean isClean = true;
+        boolean isAvailable = true;
+        int numberOfBeds = 2;
+        Room r = new Room(isClean,isAvailable,numberOfBeds);
+    }
+}
---
+++ hotel-application/src/main/java/Room.java
@@ -0,0 +1,12 @@
+public class Room {
+    boolean isClean;
+    boolean isAvailable;
+    int numberOfbeds;
+    // create constructor
+
+    public Room(boolean isClean, boolean isAvailable, int numberOfbeds) {
+        this.isClean = isClean;
+        this.isAvailable = isAvailable;
+        this.numberOfbeds = numberOfbeds;
+    }
+}
