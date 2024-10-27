151d88bc53f01629be4f57b454a9a17695782b4b
Ayanna Williams <ayanna.williams20@att.net>
2024-10-26 21:30:34 -0400

    phase 1

--- car-dealership/src/main/java/com/pluralsight/Dealership.java
+++ car-dealership/src/main/java/com/pluralsight/Dealership.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class Dealership {
@@ -13,6 +14,52 @@ public class Dealership {
         this.name = name;
         this.address = address;
         this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min,double max){
+        ArrayList<Vehicle> filteredByPrice;
+        filteredByPrice = new ArrayList<Vehicle>();
+        for (Vehicle vehicle: inventory){
+
+        }
+        return filteredByPrice; } //returns arrayList
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model){
+        ArrayList<Vehicle> filteredByMakeModel;
+        filteredByMakeModel = new ArrayList<Vehicle>();
+                return filteredByMakeModel;
+    }
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max){
+        ArrayList<Vehicle> filteredbyYear;
+        filteredbyYear = new ArrayList<Vehicle>();
+        return filteredbyYear;
+    }
+    public ArrayList<Vehicle> getVehiclesByColor(String color){
+        ArrayList<Vehicle> filteredByColor;
+        filteredByColor = new ArrayList<Vehicle>();
+        return filteredByColor;
+    }
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max){
+        ArrayList<Vehicle> filteredByMileage;
+        filteredByMileage = new ArrayList<Vehicle>();
+        return filteredByMileage;
+    }
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType){
+        ArrayList<Vehicle> filteredByType;
+        filteredByType = new ArrayList<Vehicle>();
+        return filteredByType;
+    }
+    public ArrayList<Vehicle> getAllVehicles(){
+        return inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+      inventory.add(vehicle);
+
+    }
+
+    public void removeVehicle(Vehicle vehicle){
+
     }
 
     public String getName() {
