ddccb86802d2f52929b1c2c8b3b998d01de4c128
Ayanna Williams <ayanna.williams20@att.net>
2024-10-29 16:18:23 -0400

    phase 1 and part of 2

---
+++ car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+public class DealershipFileManager {
+
+    private static final String filePath="inventory.csv";
+
+    public Dealership getDealership(){
+        Dealership myDealership = new Dealership("name","address","phone");
+        return Dealership;
+    }
+    public void saveDealership(String dealership){
+
+    }
+
+}
