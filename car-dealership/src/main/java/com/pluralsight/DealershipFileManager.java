6c3d6d6877ff82239c4d567c391544843d7c333b
Ayanna Williams <ayanna.williams20@att.net>
2024-10-31 20:33:39 -0400

    progress

--- car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,11 +1,28 @@
 package com.pluralsight;
-public class DealershipFileManager {
 
-    private static final String filePath="inventory.csv";
+import java.io.BufferedReader;
+import java.io.FileReader;
+public class DealershipFileManager {
+    public static void main(String[] args) {
+        new DealershipFileManager().getDealership();
+    }
+    private static final String filePath="src/main/resources/inventory.csv";
 
     public Dealership getDealership(){
-        Dealership myDealership = new Dealership("name","address","phone");
-        return Dealership;
+       // Dealership myDealership = new Dealership("name","address","phone");
+       try {
+           FileReader fr = new FileReader(filePath);
+           BufferedReader br = new BufferedReader(fr);
+           String dealershipInfo = br.readLine();
+           String[] tokens=dealershipInfo.split("[|]");
+           System.out.println(tokens[0]);
+           return new Dealership(tokens[0],tokens[1],tokens[2]);
+           //for loop to split  while br.readline() !null
+       }
+       catch(Exception e){
+
+       }
+       return null;
     }
     public void saveDealership(String dealership){
 
