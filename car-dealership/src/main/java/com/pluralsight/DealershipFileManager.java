e5e5f258459f0fcdf12aade3d49e4c4f0dce3899
Ayanna Williams <ayanna.williams20@att.net>
2024-11-01 16:43:33 -0400

    read and load csv

--- car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -7,16 +7,26 @@ public class DealershipFileManager {
         new DealershipFileManager().getDealership();
     }
     private static final String filePath="src/main/resources/inventory.csv";
+    String delimiter = "[|]";
 
     public Dealership getDealership(){
-       // Dealership myDealership = new Dealership("name","address","phone");
+       Dealership myDealership = null;
        try {
            FileReader fr = new FileReader(filePath);
            BufferedReader br = new BufferedReader(fr);
            String dealershipInfo = br.readLine();
            String[] tokens=dealershipInfo.split("[|]");
-           System.out.println(tokens[0]);
-           return new Dealership(tokens[0],tokens[1],tokens[2]);
+           System.out.println(tokens[0] +"|"+ tokens[1] +"|"+tokens[2]);
+
+           String vehicleInfo;
+           myDealership = new Dealership(tokens[0],tokens[1],tokens[2]);
+           while((vehicleInfo= br.readLine())!=null){
+               String[] vehicleTokens = vehicleInfo.split("[|]");
+               Vehicle vehicle = new Vehicle(Integer.parseInt(vehicleTokens[0]),Integer.parseInt(vehicleTokens[1]),vehicleTokens[2],vehicleTokens[3], vehicleTokens[4],vehicleTokens[5],Integer.parseInt(vehicleTokens[6]),Double.parseDouble(vehicleTokens[7]));
+               //public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+               myDealership.addVehicle(vehicle);
+               System.out.println(vehicle);
+           }
            //for loop to split  while br.readline() !null
        }
        catch(Exception e){
