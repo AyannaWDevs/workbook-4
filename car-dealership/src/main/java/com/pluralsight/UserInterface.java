ec6d0f9b25dd7d3c323241dc08ff36e63564be7d
Ayanna Williams <ayanna.williams20@att.net>
2024-11-02 18:15:04 -0400

    Phase 3

---
+++ car-dealership/src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+public class UserInterface {
+    Dealership dealership;
+}
+public UserInterface(){
+
+}
+public void display(){
+}
+public void processGetByPriceRequest(){
+
+}
+public void processGetByMakeModelRequest(){
+
+}
+public void processGetByYearRequest(){
+
+}
+public void processGetByColorReqest(){
+
+}
+public void processGetByMileageRequest(){
+
+}
+public void processGetByVehicleTypeRequest(){
+
+}
+public void processGetAllVehiclesRequest(){
+
+}
+public void processAddVehicleRequest(){
+
+}
+public void processRemoveVehicleRequest(){
+    
+}
+
