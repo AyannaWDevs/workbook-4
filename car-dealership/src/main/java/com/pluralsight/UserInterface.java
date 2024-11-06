305c42cad89f1f76f255d3b1e7411bc9ff1ad770
Ayanna Williams <ayanna.williams20@att.net>
2024-11-06 14:02:23 -0500

    progress

--- car-dealership/src/main/java/com/pluralsight/UserInterface.java
+++ car-dealership/src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
     Dealership dealership;
 }
@@ -7,6 +9,68 @@ public UserInterface(){
 
 }
 public void display(){
+private init();
+    Scanner scanner = new Scanner(System.in);
+    userChoice = usersMenuInput(scanner);
+    System.out.println("""
+                Please enter the number corresponding to your choice:
+                1) List all Vehicles
+                2) Search for vehicles by price 
+                3) Search for Vehicles by make / model
+                4) Search vehicles by color
+                5) Search for vehicles by mileage
+                6) Search vehicles by type (car, truck, SUV, van)
+                7 ) Search vehicles by year 
+                8) Add a vehicle
+                9 ) Remove a vehicle
+                10) Sell/Lease a Vehicle
+                x ) Quit
+                """);
+    do {
+        // switch statement to process users choice
+        switch (userChoice.toLowerCase()){
+            case "1":
+                processGetAllVehiclesRequest();
+                break;
+            case "2":
+                processGetByPriceRequest();
+                break;
+            case "3":
+                processGetByMakeModelRequest();
+                break;
+            case "4":
+                processGetByColorRequest();
+                break;
+            case "5":
+                processGetByMileageRequest();
+                break;
+            case "6":
+                processGetByVehicleTypeRequest();
+                break;
+            case "7":
+                processGetByYearRequest();
+                break;
+            case "8":
+                processAddVehicleRequest();
+                break;
+            case "9":
+                processRemoveVehicleRequest();
+                break;
+            case "10":
+                displaySellLeaseMenu();
+                break;
+            case "x":
+                System.out.println("Exiting to main menu");
+                break;
+            default:
+                System.out.println("Invalid input try again.");
+        }
+        // stops loop once "x" is input
+    } while (!choice.equalsIgnoreCase("x"));
+    scanner.close();
+}
+private void init(){
+
 }
 public void processGetByPriceRequest(){
 
@@ -17,7 +81,7 @@ public void processGetByMakeModelRequest(){
 public void processGetByYearRequest(){
 
 }
-public void processGetByColorReqest(){
+public void processGetByColorRequest(){
 
 }
 public void processGetByMileageRequest(){
@@ -33,6 +97,6 @@ public void processAddVehicleRequest(){
 
 }
 public void processRemoveVehicleRequest(){
-    
+
 }
 
